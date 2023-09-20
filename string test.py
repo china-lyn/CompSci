6801024a821417d3dbdae44fa68f863a437e2a2f
china-lyn <144444810+china-lyn@users.noreply.github.com>
2023-09-20 12:32:21 +0100

    Add files via upload

---
+++ string test.py
@@ -0,0 +1,18 @@
+string = 'china'
+print(string[2])
+print(string[-1])
+print(string[:4])
+print(string.upper())
+print(string.lower())
+n = 'n'
+print(string.count(n))
+print(string.find(n))
+print(string.replace(n,'c'))
+bool(string)
+print(string.islower())
+print(string.isupper())
+print(string.isalnum()) # alphanumeric
+print(string.isalpha()) # alphebetic
+print(string.isdigit())
+print(string.index('h'))
+print(string.strip())
