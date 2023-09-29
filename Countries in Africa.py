9ca904dc247b82f2ea4478277ff9eed99291703c
china-lyn <144444810+china-lyn@users.noreply.github.com>
2023-09-29 11:58:19 +0100

    Add files via upload

---
+++ Countries in Africa.py
@@ -0,0 +1,21 @@
+lives = 3
+countries = ['Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad', 'Comoros', 'Ivory Coast', 'Djibouti', 'Democratic Republic Of The Congo', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Republic Of The Congo', 'Rwanda', 'Sao Tome & Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe' ]
+
+print('---Countries of Africa---')
+score = 0
+while len(countries) > 0:
+    print(len(countries), 'countries left')
+    print('Score:', score)
+    print('Lives:', lives)
+    country = input('Enter the name of a country in Africa: ')
+    if country.title() in countries:
+        print('Good guess!')
+        score = score + 1
+        countries.remove(country.title())
+    else:
+        print('Invalid guess')
+        lives = lives - 1
+    if lives == 0 or countries == 0:
+        print ('Game Over')
+        break 
+            
