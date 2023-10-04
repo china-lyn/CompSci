661178c7df2379d5735b41c47b67af7fe00952b3
china-lyn <19clynch.acc@lwetb.ie>
2023-10-04 22:00:15 +0100

    Update Countries in Africa.py

--- Countries in Africa.py
+++ Countries in Africa.py
@@ -1,5 +1,6 @@
 lives = 3
 countries = ['Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad', 'Comoros', 'Ivory Coast', 'Djibouti', 'Democratic Republic Of The Congo', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Republic Of The Congo', 'Rwanda', 'Sao Tome & Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe' ]
+guessed = []
 
 print('---Countries of Africa---')
 score = 0
@@ -12,6 +13,9 @@ while len(countries) > 0:
         print('Good guess!')
         score = score + 1
         countries.remove(country.title())
+        guessed.append(country.title())
+    elif country.title() in guessed:
+        print('You already guessed that!')
     else:
         print('Invalid guess')
         lives = lives - 1
