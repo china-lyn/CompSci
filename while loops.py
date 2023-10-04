ff0139a3ebd89ff31cca85fee9e15cd6c0e02dda
china-lyn <19clynch.acc@lwetb.ie>
2023-10-04 22:46:09 +0100

    Update while loops.py

--- while loops.py
+++ while loops.py
@@ -2,37 +2,28 @@
 total = 0
 while total <= 50:
     num = int(input('Enter a number: '))
-    total = num + total 
+    total =+ num
     print('The total is', total)
     
     
 #046
-usernum = int(input('Enter a number: '))
+usernum = 0
 while usernum < 5:
     usernum = int(input('Enter a number: '))
-    if usernum > 5:
-        break
 print('The last number you entered was a', usernum)
 
 #047
 num1 = int(input('Enter a number: '))
 num2 = int(input('Enter a second number: '))
-num_ = input('Do you want to enter another number?: ')
-while num_.lower() == 'yes':
+while input('Enter "y" if you want to enter another number, any other letter to continue: ') == 'y':
     num3 = int(input('Enter another number: '))
     total12 = num1 + num2 + num3
-    num_ = input('Do you want to enter another number?: ')
-    if num_.lower() == 'no':
-        break
 print('The total is', total12)
+
 #048
 namecount = 0
-name = input('Give me the name of somebody you want to invite to a party: ')
-print(name.title(),'has now been invited')
-namecount = namecount + 1
-invite = input('Do you want to invite someone else?: ')
-while invite.lower() == 'yes':
-    name = input('Give me the name of somebody else you want to invite to a party: ')
+while True:
+    name = input('Give me the name of somebody you want to invite to a party: ')
     print(name.title(),'has now been invited')
     namecount = namecount + 1
     invite = input('Do you want to invite someone else?: ')
@@ -40,17 +31,19 @@ while invite.lower() == 'yes':
         break
 print(namecount, 'people are coming to the party!')
 
-# #049
+#049
 compnum = 50
-number = int(input('Guess a number: '))
 guess = 0
+number = -1
+
 while number != compnum:
+    guess = guess + 1
+    number = int(input('Guess a number: '))
     if number < compnum:
         print('Your number is too low!')
-    else:
+    elif number > compnum:
         print('Your number is too high!')
-    number = int(input('Guess again: ')) #*****
-    guess = guess + 1
+    
 print('Well done, you took', guess, 'attempts!')
 
 
@@ -68,13 +61,11 @@ print('Thank you :)')
 #051
 bottle = 10
 while bottle != 0:
-    print('There are', bottle, 'green bottles hanging on the wall and if 1 green bottle should accidentally fall,')
-    bottle = bottle - 1
+    print('\nThere are', bottle, 'green bottles hanging on the wall and if 1 green bottle should accidentally fall,')
     many = int(input('how many bottles green bottles will be hanging on the wall?: '))
-    if many == bottle:
+    if many == bottle - 1:
+        bottle = bottle - 1
         print('There will be', bottle, 'green bottles hanging on the wall!')
     else:
-        many2 = int(input('No, try again: '))
+        print('No, try again')
 print('There are no more green bottles hanging on the wall')
-    
-    
