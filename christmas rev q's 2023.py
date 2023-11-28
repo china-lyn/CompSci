9b1257f53b7b6df67fe6bff469d9a90867ac217a
china-lyn <144444810+china-lyn@users.noreply.github.com>
2023-11-28 21:53:47 +0000

    Add files via upload

---
+++ christmas rev q's 2023.py
@@ -0,0 +1,73 @@
+# 1
+name = input('Enter your name: ')
+print(f'Hello {name}')
+
+# 2
+# num = int(input('Enter a number: '))
+# num2 = int(input('Enter a second number: '))
+# sum = num + num2
+# print(f'The total is: {sum}')
+
+
+# 3
+# tempc = int(input('Enter a temperature in Celsius: '))
+# tempf = 9 / 5 * tempc + 32
+# print(f'{tempc} in Farenheit is {tempf}')
+
+
+# 4
+# p = int(input('Enter a principle: '))
+# r = int(input('Enter a rate: '))
+# t = int(input('Enter a time: '))
+# simpint = p * r * t / 100
+# print(simpint)
+
+# 5 ******
+# secs = int(input('Enter seconds: '))
+# 
+# hours = secs // 3600
+# rm = secs - hours * 3600
+# 
+# min = rm // 60
+# sec = rm - min * 60
+# 
+# print(f'Hours: {hours}')
+# print(f'Minutes: {min}')
+# print(f'Seconds: {sec}')
+
+# 6
+# num1 = input('Enter a number: ')
+# num2 = input('Enter a second number: ')
+# print(num1.replace(num1, num2))
+# print(num2.replace(num2, num1))
+
+# 8
+# r = int(input('Enter the radius of a circle: '))
+# a = 3.14 * r ** 2
+# c = 2 * 3.14 * r
+# print(f'The area is {a}')
+# print(f'The circumference is {c}!')
+
+# 9
+# l = int(input('Enter the length of a rectangle: '))
+# w = int(input('Enter the width of the rectangle: '))
+# print(f'The area is {l * w}')
+# print(f'The circumference is {2 * (l + w)}')
+
+# 10
+# import math as m
+# 
+# a = int(input('Enter the length of the 1st side of a triangle: '))
+# b = int(input('Enter the length of the 2nd side: '))
+# c = int(input('Enter the length of the 3rd side: '))
+# s = (a + b + c) / 2
+# v = s * (s - a) * (s - b) * (s - c)
+# ar = m.sqrt(abs(v))
+# print(f'The area is {ar}')
+
+# 11
+# p = int(input('Enter a principle: '))
+# r = int(input('Enter a rate: '))
+# t = int(input('Enter a time: '))
+# compint = p * (1 + r / 100) ** t - p
+# print(f'The compound interest is {compint}')
